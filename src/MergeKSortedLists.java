07be481cc9780d575a7d8d5e1db405cc341a7184
Samuel Chen <samuel.chen@backstopsolutions.com>
2019-03-20 03:26:49 -0500

    add solution: merge sorted lists (divide and conquer)

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -9,6 +9,7 @@ public class MergeKSortedLists {
      * use priorityQueue
      */
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
         // define priorityQueue
         PriorityQueue<ListNode> q= new PriorityQueue<ListNode>(lists.length , new Comparator<ListNode>(){
             @Override
@@ -39,6 +40,45 @@ public class MergeKSortedLists {
             }
             temp = temp.next;
         }
+        return dummy.next;
+    }
+
+    public ListNode divideAndConquer(ListNode[] lists, int start, int end) {
+        //return final situation
+        if (start == end) {
+            return lists[start];
+        } else if (start == end - 1) {
+            return merge2Lists(lists[start], lists[end]);
+        } else if (start > end) {
+            return null;
+        }
+
+        //divide
+        ListNode a = divideAndConquer(lists, start, start + (end - start) / 2);
+        ListNode b = divideAndConquer(lists, start + (end - start) / 2 + 1, end);
+
+        return merge2Lists(a, b);
+    }
+
+    public ListNode merge2Lists(ListNode first, ListNode second) {
+        if (second == null) return first;
+        if (first == null) return second;
+
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                head.next = first;
+                first = first.next;
+            } else {
+                head.next = second;
+                second = second.next;
+            }
+            head = head.next;
+        }
+        if (first != null) head.next = first;
+        if (second != null) head.next = second;
+
         return dummy.next;
     }
 
--- test/MergeKSortedListsTest.java
+++ test/MergeKSortedListsTest.java
@@ -12,5 +12,16 @@ public class MergeKSortedListsTest {
         MergeKSortedLists msl = new MergeKSortedLists();
         ListNode result = msl.mergeKLists(lists);
         assertEquals(ListNode.createLinkNodes(1,1,2,3,4,4,5,6), result);
+        assertEquals(ListNode.createLinkNodes(null), msl.mergeKLists(new ListNode[0]));
+    }
+
+    @Test
+    public void divideAndConquer() {
+        ListNode[] lists = new ListNode[]{ ListNode.createLinkNodes(1, 4, 5),
+                ListNode.createLinkNodes(1, 3, 4), ListNode.createLinkNodes(2,6)};
+        MergeKSortedLists msl = new MergeKSortedLists();
+        ListNode result = msl.divideAndConquer(lists, 0, lists.length - 1);
+        assertEquals(ListNode.createLinkNodes(1,1,2,3,4,4,5,6), result);
+        assertEquals(ListNode.createLinkNodes(null), msl.divideAndConquer(new ListNode[0],0, -1));
     }
 }
