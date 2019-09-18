39dbbdb26f6252c1fc62fa67315359105eab01ed
chenhy <410775541@qq.com>
2019-09-18 13:20:26 +0800

    validate sudoku

---
+++ src/ValidSudoku.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author <a herf="chenhy@itek-china.com">陈海越</a>
+ * @version 1.0
+ * @since 新标准版5.0
+ *
+ * <pre>
+ * 历史：
+ *      建立: 2019/9/18 陈海越
+ *        </pre>
+ */
+public class ValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet();
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char number = board[i][j];
+                if (number != '.')
+                    if (!seen.add(number + " in row " + i) ||
+                            !seen.add(number + " in column " + j) ||
+                            !seen.add(number + " in block " + i / 3 + "-" + j / 3))
+                        return false;
+            }
+        }
+        return true;
+    }
+}
